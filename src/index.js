baebef9d7fd511d569e8e4b6acac5c53330185a6
nfnt-rn <infantreni@protonmail.com>
2022-06-07 02:19:04 +0530

    up

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ var root = document.body;
 m.mount(root, function () {
   return {
     view: function (vnode) {
-      return m("div", { class: "container" }, [m(Header), m(Graph)]);
+      return m("div", [
+        m("div", { class: "container" }, [m(Header), m(Graph)]),
+        m(
+          "footer",
+          { class: "footer" },
+          "© Copyright 2022. Expenses chart component clone made with mithriljs/stylus by nfnt."
+        ),
+      ]);
     },
   };
 });
